6c900e5e283870bfaa41e4c05c4f92518fafe3c9
Ihezuo Divine Chikezie <ihezuodivine@gmail.com>
2025-08-26 07:26:02 +0100

    initial and setup

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Item
+
+
+def item_list(request):
+    context = {
+        'item': Item.objects.all()
+    }
+    return render(request, "Main.html", context)
